f0396b2a3f5779fc3e0e60ad8f0a1ef09aab991a
VicenteMdz <vicente.d.llanos@gmail.com>
2024-04-02 12:16:59 -0300

    trabajo sobre el componente egreso-componet

--- app_de_presupuesto/src/app/egreso/egreso.servicio.ts
+++ app_de_presupuesto/src/app/egreso/egreso.servicio.ts
@@ -4,5 +4,11 @@ export class EgresoServicio{
     egresos: Egreso[] = [
         new Egreso("Renta Depto", 900),
         new Egreso("Ropa", 200)
-    ]
+    ];
+
+
+    eliminar(egreso: Egreso) {
+      const indice: number = this.egresos.indexOf(egreso);
+      this.egresos.splice(indice, 1);
+    }
 }
