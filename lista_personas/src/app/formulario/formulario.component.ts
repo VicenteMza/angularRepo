a60f429afbee5cdb8d90bf7169f7f12d11b27c19
VicenteMdz <vicente.d.llanos@gmail.com>
2024-04-01 22:30:46 -0300

    lista de personas en angular

---
+++ lista_personas/src/app/formulario/formulario.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoggingService } from '../LoggingService.service';
+import { Persona } from '../persona.model';
+import { PersonasService } from '../personas.service';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent  {
+  nombreInput:string;
+  apellidoInput:string;
+
+  constructor(private loggingService:LoggingService,
+              private personasService:PersonasService,){
+                this.personasService.saludar.subscribe(
+                  (indice: number)=> alert("El indice es:" + indice)
+                );
+              }
+
+  agregarPersona(){
+    let persona1 = new Persona(
+      this.nombreInput, 
+      this.apellidoInput);
+    this.personasService.agregarPersona(persona1);
+  }
+}
